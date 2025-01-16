257518f100fa079c701cc4b6d7b41a3ac4683844
Rohan Raj <142926169+rohnraj@users.noreply.github.com>
2025-01-16 11:20:57 +0000

    #

---
+++ Basics_01/conversion/sript.js
@@ -0,0 +1,8 @@
+let stringNumber= true
+let actualNumber=Number(stringNumber)
+
+// console.log(typeof actualNumber, actualNumber)
+
+let stringBool=0
+let boolconv=Boolean(stringBool)
+console.log(typeof boolconv, boolconv)
