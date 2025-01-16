73d657b4a24e1bd6a5d45dff790d06dac4ea142f
Rohan Raj <142926169+rohnraj@users.noreply.github.com>
2025-01-16 08:52:42 +0000

    declarative in js commit

---
+++ Basics_01/variable/script.js
@@ -0,0 +1,10 @@
+const userName="Rohan"
+let password="12345"
+var age=22
+countery='INDIA'
+// const a; const should be define at a time of decalre
+let b;
+var c;
+
+
+console.log([userName, password, age, countery, b, c])
