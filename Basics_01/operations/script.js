afe8853ca40d0a63db11eff8e8dd7c3dd4fe2d92
Rohan Raj <142926169+rohnraj@users.noreply.github.com>
2025-01-18 09:20:10 +0000

    #

---
+++ Basics_01/operations/script.js
@@ -0,0 +1,33 @@
+// console.log("rohan" + "raj");
+
+// console.log(1+2+"3")
+
+// console.log("1"+2+3);
+
+// console.log(true + 2);
+
+// console.log(3+false);
+
+// console.log(+true);
+
+// console.log(+"");
+
+
+//*******operations******** */
+
+//same data types
+console.log(2>1);
+
+//different data types
+console.log("2"<3);
+
+console.log(3>true);
+
+//imp
+console.log(null<3);
+console.log(null==0);
+console.log(null>=0);
+
+
+
+
